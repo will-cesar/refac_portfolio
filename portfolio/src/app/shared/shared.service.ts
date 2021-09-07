d3cabb8541f877cc70d415ada620549611562d5f
will-cesar <willcesar010421@gmail.com>
2021-09-07 13:11:30 -0300

    atualizacoes de links e curriculo

--- portfolio/src/app/shared/shared.service.ts
+++ portfolio/src/app/shared/shared.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SharedService {
+  constructor() {}
 
-  constructor() { }
-
-  scrollTo(elementId: string) {
-    document.getElementById(elementId).scrollIntoView({ behavior: "smooth", block: "start" });
+  scrollTo(elementId: string): void {
+    document
+      .getElementById(elementId)
+      .scrollIntoView({ behavior: 'smooth', block: 'start' });
   }
 
-  downloadPdf() {
-    window.open('../../assets/pdfs/william-cesar-andrade-da-rocha.pdf', 'blank');
+  downloadPdf(): void {
+    window.open('../../assets/pdfs/curriculo-william-cesar.pdf', 'blank');
   }
 }
